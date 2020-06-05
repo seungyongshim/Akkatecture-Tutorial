48a4d1052b6c2b44aaf9ab090b39b293476048ac
심승용 <seungyong.shim@mirero.co.kr>
2020-06-05 15:12:42 +0900

    https://akkatecture.net/docs/walkthrough-introduction

---
+++ Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Akkatecture_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
